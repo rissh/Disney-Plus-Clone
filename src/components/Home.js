63cefcb41fd8ff158edf855372f030cea0a317de
Rishikesh <rishikeshjagadale19@gmail.com>
2022-11-03 17:36:04 +0530

    Initial Commit

---
+++ src/components/Home.js
@@ -0,0 +1,51 @@
+import React, {useEffect} from 'react'
+import styled from 'styled-components'
+import SlickSlider from './SlickSlider'
+import Channels from './Channels'
+import Movies from './Movies'
+import db from '../firebase'
+
+
+
+
+function Home() {
+
+    useEffect(() => {
+        db.collection("movies").onSnapshot((snapshot) => {
+            let tempMovies = snapshot.docs.map((doc) => {
+                return { id: doc.id, ...doc.data()}
+            })
+            
+        }) 
+    }, [])
+   
+
+    return (
+        <Container>
+            <SlickSlider />
+            <Channels />
+            <Movies />
+        </Container>    
+    )
+}
+
+export default Home
+
+const Container = styled.main`
+    min-height: calc(100vh - 70px);
+    padding: 0 calc(3.5vw + 5px);
+    position: relative;
+    overflow-x: hidden;
+
+    &:before{
+        background: url("/images/home-background.png") center center / cover
+        no-repeat fixed;
+        content: "";
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        z-index: -1;
+    }
+`
